b17283d57a8d9a6d46de873fb3285b0e8a9d7e4e
Abin Joseph <itsmeabinj@gmail.com>
2020-10-26 01:12:05 +0530

    Rectangil funtion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("God is love");
+let rectangle ={
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) =>(x*y)
+}
+
+let solvRectangle = (l,b)=>{
+    if(l> 0 && b>0){
+        console.log(`The  Perimeter Of the rectangle is :${rectangle.perimeter(l,b)}`)
+        console.log(`The Area Of the rectangle is :${rectangle.area(l,b)}`)       
+    }
+    else{
+        console.log("The rectangle Dimension must be greater than zero");
+    }
+}
+solvRectangle(0,30)
